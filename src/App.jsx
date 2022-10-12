45898ce690740b13a55b686abf6e76ff39b427bc
Kamlakar Tiwari <kamlakart12@gmail.com>
2022-10-13 01:56:40 +0530

    added checkout/back button

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import ScrollToTop from './components/common/ScrollToTop';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="container">
       <BrowserRouter>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/cart' element={<Cart />} />
-          <Route path='*' element={<Home />} />
-        </Routes>
+        <ScrollToTop>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/cart' element={<Cart />} />
+            <Route path='*' element={<Home />} />
+          </Routes>
+        </ScrollToTop>
       </BrowserRouter>
     </div>
   );
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -6,9 +6,11 @@ import EmptyCart from '../../components/Cart/EmptyCart';
 import CartMenu from '../../components/common/CartMenu';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCartTotal } from '../../redux/features/cartSlice';
+import { useNavigate } from 'react-router-dom';
 const Cart = () => {
-  const { totalAmount, items } = useSelector((state) => state.cart);
+  const { totalAmount, items, totalCount } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(getCartTotal());
   }, [items]);
@@ -17,14 +19,20 @@ const Cart = () => {
       <div className="cart-header">
         <Logo />
       </div>
-      {totalAmount === 0 ? <EmptyCart /> : 
-      <div className="orders">
-        <div className="orders-heading"><h1>Your Orders</h1></div>
-        <div className="orders-menu">
-          <CartMenu list={items} />
-        </div>
-        <h3 className="orders-total">Your Total ${totalAmount}</h3>
-      </div>}
+      {totalAmount === 0 ? <EmptyCart /> :
+        <div className="orders">
+          <div className="back-btn" onClick={() => navigate('/')}>
+            <button><i className="fas fa-long-arrow-alt-left"></i> Back</button>
+          </div>
+          <div className="orders-heading"><h1>Your Orders</h1></div>
+          <div className="orders-menu">
+            <CartMenu list={items} />
+          </div>
+          <h3 className="orders-total">Your Total ${totalAmount}</h3>
+          <div className="Checkoutbtn-container">
+            <button className='checkout-btn'>Proceed to Buy ({totalCount} {totalCount>1?<>items</>:<>item</>}) <i className="fas fa-long-arrow-alt-right"></i></button>
+          </div>
+        </div>}
       <Footer />
     </div>
   )
