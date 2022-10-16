76e1aade75154cf15ba04d0b55cf7d4631191875
kamlakart <88089097+kamlakart@users.noreply.github.com>
2022-10-16 23:22:38 +0530

    Fixed Some Bugs..

--- src/components/common/MenuFilter/FilterByType/index.jsx
+++ src/components/common/MenuFilter/FilterByType/index.jsx
@@ -9,9 +9,9 @@ const FilterByType = () => {
         <div className="filterByMeal">
             <h3>Meals</h3>
             <div className="filterByMeal-items">
-                <h4 onClick={() => dispatch(setType('Lunch'))}>Lunch <i class="fa-solid fa-mug-saucer"></i></h4>
-                <h4 onClick={() => dispatch(setType('Breakfast'))}>Breakfast <i class="fa-solid fa-bowl-food"></i></h4>
-                <h4 onClick={() => dispatch(setType('Fastfood'))}>Fastfood <i class="fa-solid fa-pizza-slice"></i></h4>
+                <h4 onClick={() => dispatch(setType('Lunch'))}>Lunch <i className="fa-solid fa-mug-saucer"></i></h4>
+                <h4 onClick={() => dispatch(setType('Breakfast'))}>Breakfast <i className="fa-solid fa-bowl-food"></i></h4>
+                <h4 onClick={() => dispatch(setType('Fastfood'))}>Fastfood <i className="fa-solid fa-pizza-slice"></i></h4>
             </div>
         </div>
     )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
 import cartReducer from "./features/cartSlice";
-import { persistReducer, persistStore } from "redux-persist";
+import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
@@ -17,6 +17,11 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
     reducer: persistedReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: {
+            ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        }
+    })
 });
 
 export const persistor = persistStore(store);
